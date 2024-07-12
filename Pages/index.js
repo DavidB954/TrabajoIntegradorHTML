d74db4d61bc1eed177a85817458c3dbd5de49e76
David <david_o_bellido@hotmail.com>
2024-07-12 19:22:59 -0400

    Add project

---
+++ Pages/index.js
@@ -0,0 +1,9 @@
+const linkedinBtn = document.querySelector('.linkedin-btn');
+
+linkedinBtn.addEventListener('click', () => {
+  window.open('https://www.linkedin.com/in/bellidodavid/', '_blank');
+});
+
+
+const botonDescarga = document.getElementById('boton-descarga');
+
